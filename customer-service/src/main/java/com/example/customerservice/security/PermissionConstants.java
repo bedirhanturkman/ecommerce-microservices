f9ff29532732e309369269a127b3dd8ce09c29bf
Bedirhan Türkman <bedirhantr15@gmail.com>
2026-07-07 14:21:50 +0300

    feat(auth): add seller role authorization

--- customer-service/src/main/java/com/example/customerservice/security/PermissionConstants.java
+++ customer-service/src/main/java/com/example/customerservice/security/PermissionConstants.java
@@ -7,8 +7,12 @@ public final class PermissionConstants {
 
     public static final String ROLE_USER = "USER";
     public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_SELLER = "SELLER";
 
     public static final String HAS_ROLE_ADMIN = "hasRole('" + ROLE_ADMIN + "')";
     public static final String HAS_ROLE_USER_OR_ADMIN =
             "hasAnyRole('" + ROLE_USER + "', '" + ROLE_ADMIN + "')";
+
+    public static final String HAS_ROLE_USER_OR_ADMIN_OR_SELLER =
+            "hasAnyRole('" + ROLE_USER + "', '" + ROLE_ADMIN + "', '" + ROLE_SELLER + "')";
 }
