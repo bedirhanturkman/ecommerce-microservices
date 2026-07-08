95c7918ed32991d89110b82fee01a056fa1a1298
Bedirhan Türkman <bedirhantr15@gmail.com>
2026-07-08 23:24:47 +0300

    fix: Restclient structure

--- order-service/src/main/java/com/example/orderservice/client/ProductClient.java
+++ order-service/src/main/java/com/example/orderservice/client/ProductClient.java
@@ -3,13 +3,16 @@ package com.example.orderservice.client;
 import com.example.orderservice.dto.ProductResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 @Component
 public class ProductClient {
 
     private final RestClient restClient;
 
-    public ProductClient(RestClient.Builder restClientBuilder) {
+    public ProductClient(
+            @Qualifier("loadBalancedRestClientBuilder") RestClient.Builder restClientBuilder
+    ) {
         this.restClient = restClientBuilder
                 .baseUrl("http://product-service")
                 .build();
